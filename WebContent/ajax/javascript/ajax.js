a17497114929f476f6e5dc898c90a356d690bf84
adminl <adminl@GTM06.home>
2015-07-08 16:34:14 +0200

    'ajax' files : added a search from 2 characters entered

--- WebContent/ajax/javascript/ajax.js
+++ WebContent/ajax/javascript/ajax.js
@@ -1,14 +1,32 @@
-var pressBouton = 0;
+function startSearch(codePostal){
+	if(codePostal==null)
+		$("#divVillesId").html("");
+	var url = "http://192.168.1.16:8080/france/rest/villes/cp/" + codePostal;
+	var listVilles = "<select>";
+	$.getJSON(url, function(data){
+		for(var ville in data){
+			listVilles += "<option>"+data[ville].codePostal+" : "+data[ville].nom+"</option>";
+		}
+		$("#divVillesId").html(listVilles+"</select>");
+	});
+}
 
 $(document).ready(function() {
-	$("#buttonId").click(function() {
-		var url = "http://192.168.1.16:8080/france/rest/villes/cp/" + $("#cpId").val();
-		var listVilles = "<select>";
-		$.getJSON(url, function(data){
-			for(var ville in data){
-				listVilles += "<option>"+data[ville].codePostal+" : "+data[ville].nom+"</option>";
-			}
-			$("#divVillesId").html(listVilles+"</select>");
-		});
+	$("#cpId").keyup(function(){
+		if($(this).val().length>1)
+			startSearch($(this).val());
+		else
+			startSearch(null);
 	});
+	
+//	$("#buttonId").click(function() {
+//		var url = "http://192.168.1.16:8080/france/rest/villes/cp/" + $("#cpId").val();
+//		var listVilles = "<select>";
+//		$.getJSON(url, function(data){
+//			for(var ville in data){
+//				listVilles += "<option>"+data[ville].codePostal+" : "+data[ville].nom+"</option>";
+//			}
+//			$("#divVillesId").html(listVilles+"</select>");
+//		});
+//	});
 });
