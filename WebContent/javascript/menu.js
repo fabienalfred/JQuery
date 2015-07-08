f0aa394219f403c8ea5106b6d95d34959840514b
adminl <adminl@GTM06.home>
2015-07-08 13:56:40 +0200

    'menu' files : mouse events with jquery + BS css

---
+++ WebContent/javascript/menu.js
@@ -0,0 +1,17 @@
+//function onMouseEnter(){
+////	$(this).css({backgroundcolor:"grey",padding:"15px",border:"5px inset black"});
+//	$("#"+$(this).attr("id")+" > ul").show();
+////	console.log($("#sub"+$(this).attr("id")).attr("id"));
+//}
+//
+//function onMouseLeave(){
+////	$(this).css({backgroundcolor:"black",padding:"10px",border:"5px outset black"});
+//	$("#"+$(this).attr("id")+" > ul").hide();
+////	$("#"+$(this).attr("id")+" > ul").css({float:"none",display:"none"});
+//}
+//
+//$(document).ready(function(){
+//	$(".menuItem").mouseenter(onMouseEnter);
+//	$(".menuItem").mouseleave(onMouseLeave);
+//	$(".subMenu").hide();
+//});
